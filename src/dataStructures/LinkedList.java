3d7253fb7c3217752c324be253247d76ee6754d1
Gerganadd <gerganadoncheva2003@gmail.com>
2021-03-23 13:07:35 +0200

    added List and tested it

---
+++ src/dataStructures/LinkedList.java
@@ -0,0 +1,184 @@
+package dataStructures;
+
+import interfaces.List;
+
+public class LinkedList<T extends Comparable<T>> implements List
+{
+	Node <T> linkedList;
+	int lenght;
+ 
+	@Override
+	public boolean isEmpty()
+	{
+		return this.linkedList == null;
+	}
+
+	@Override
+	public boolean contains(Object item) 
+	{
+		Node<T> current = this.linkedList;
+		
+		while(current != null)
+		{
+			if (current.data.equals(item))
+			{
+				return true;
+			}
+			current = current.next;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public void add(Object item) 
+	{
+		if (isEmpty())
+		{
+			this.linkedList = new Node(item);
+		}
+		else
+		{
+			Node<T> current = this.linkedList;
+			
+			while (current.next != null)
+			{
+				current = current.next;
+			}
+			
+			current.next = new Node(item, null);
+		}
+		
+		this.lenght++;
+	}
+
+	@Override
+	public void insertAt(Object item, int index) 
+	{
+		this.checkIndex(index);
+		this.lenght++;
+		
+		if (index == 0)
+		{
+			this.linkedList = new Node(item, this.linkedList);
+			return;
+		}
+		
+		Node<T> previousNode = this.findPreviousNode(index);
+		previousNode.next = new Node((T) item, previousNode.next);
+	}
+
+	@Override
+	public void removeFrom(int index) 
+	{
+		this.checkIndex(index);
+		if (isEmpty())
+		{
+			throw new NullPointerException("The list is empty");
+		}
+		
+		this.lenght--;
+		
+		if (index == 0)
+		{
+			this.linkedList = linkedList.next;
+			return;
+		}
+		
+		Node<T> previousNode = this.findPreviousNode(index);
+		
+		previousNode.next = previousNode.next.next;
+		
+		return;
+	}
+
+	@Override
+	public void remove(Object item)
+	{
+		if (isEmpty())
+		{
+			throw new NullPointerException("The list is empty");
+		}
+		
+		if (!this.contains(item)) 
+		{
+			throw new IllegalArgumentException("The list doesn't contains this item");
+			
+		}
+		
+		Node<T> current = this.linkedList;
+		int index = 0;
+		
+		while(current != null && current.data != item) 
+		{
+			current = current.next;
+			index++;
+		}
+		
+		this.removeFrom(index); 
+		
+		this.lenght--;
+	}
+
+	@Override
+	public T get(int index) 
+	{
+		checkIndex(index);
+		
+		if (isEmpty())
+		{
+			throw new NullPointerException("The list is empty");
+		}
+		
+		if (index == 0)
+		{
+			return this.linkedList.data;
+		}
+		
+		Node<T> previous = this.findPreviousNode(index);
+		
+		return previous.next.data;
+	}
+
+	@Override
+	public void clear() 
+	{
+		this.linkedList = null;
+	}
+	
+	public void print() 
+	{
+		Node<T> current = this.linkedList;
+		
+		while(current != null)
+		{
+			System.out.println(current.data);
+			current = current.next;
+		}
+	}
+	
+	private Node<T> findPreviousNode(int index)
+	{
+		this.checkIndex(index);
+
+		Node<T> previousNode = this.linkedList;
+		
+		int i = 0;
+		while(i != (index - 1))
+		{
+			previousNode = previousNode.next;
+			i++;
+		}
+		
+		return previousNode;
+	}
+	
+	private void checkIndex(int index) 
+	{
+		if(index < 0) 
+			throw new IllegalArgumentException("index must be bigger than 0!");
+		
+		if(index > lenght)
+			throw new IllegalArgumentException("index must be less than the length of list!");
+	}
+}
--- src/tests/TestDataStructures.java
+++ src/tests/TestDataStructures.java
@@ -1,12 +1,21 @@
 package tests;
 
 import dataStructures.Deck;
+import dataStructures.LinkedList;
 
 public class TestDataStructures 
 {
 
 	public static void main(String[] args) 
 	{
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(17);
+		list.add(23);
+		list.insertAt(11, 1);
+		
+		list.print();
+		
+		/*
 		Deck<Integer> d = new Deck<>();
 		
 		d.addleft(123);
@@ -18,7 +27,7 @@ public class TestDataStructures
 		
 		System.out.println("left: " + d.getLeft());
 		System.out.println("right: " + d.getRight());
-	
+		*/
 	}
 
 }
